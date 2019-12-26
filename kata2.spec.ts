35dc1cd27769091d57ffa938763d72f7d80752ca
Lincoln de Sousa <lincoln@comum.org>
2019-12-26 12:13:46 -0500

    refactor(kata2): Simpler `or' test, some renaming & better comments

--- kata2.spec.ts
+++ kata2.spec.ts
@@ -28,12 +28,14 @@ describe("#2 Kata - Matching Expressions", () => {
       expect(parser.getCurrent()).toBe('b');
     });
 
-    it("should work with any matcher", () => {
+    it("should work with any matching function", () => {
       // Given a base parser with some input
       const parser = new BaseParser();
       parser.setInput("1234abcd");
-      // And given a function that
-      const integers = () => {
+      // And given a custom matching function that parses a
+      // single digit if it's a valid digit or raises a parsing
+      // error
+      const expectInteger = () => {
         // Check if the current char is a digit and move
         // the cursor forward if so and move on
         const current = parser.getCurrent();
@@ -44,7 +46,7 @@ describe("#2 Kata - Matching Expressions", () => {
         throw new ParsingError('Not Int');
       };
       // When zero or more integers are consumed
-      const output = parser.zeroOrMore(integers);
+      const output = parser.zeroOrMore(expectInteger);
       // Then it should consume all the input since it all matches
       expect(output).toEqual(['1', '2', '3', '4']);
     });
@@ -52,6 +54,20 @@ describe("#2 Kata - Matching Expressions", () => {
 
   describe("or", () => {
     it("should choose the first expression that succeeds", () => {
+      // Given a new base parser with some input
+      const parser = new BaseParser();
+      parser.setInput("abacate");
+      // When faced with a list of options, where just the last one
+      // matches successfuly
+      const output = parser.or([
+        () => parser.expect("b"),
+        () => parser.expect("a"),
+      ]);
+      // Then it should match the successful one
+      expect(output).toBe("a");
+    });
+
+    it("should choose properly reset the cursor after a failed attempt", () => {
       // Given a new base parser with some input
       const parser = new BaseParser();
       parser.setInput("abacate");
@@ -70,12 +86,17 @@ describe("#2 Kata - Matching Expressions", () => {
       // Given a new base parser with some input
       const parser = new BaseParser();
       parser.setInput("abacate");
-      // When faced with a list of options
-      expect(() => parser.or([() => parser.error("a"), () => parser.error("b")]))
-        .toThrow(new ParsingError('No option found'));
+      // When faced with a list of options that contain no successful
+      // parsing; Then it should fail with the error `No option found'.
+      expect(() => parser.or([
+        () => { throw new ParsingError('err1'); },
+        () => { throw new ParsingError('err2'); },
+      ])).toThrow(new ParsingError('No option found'));
     });
   });
 
+  // the method `oneOrMore` is a syntax sugar for calling the matching
+  // function once and then calling it with `zeroOrMore`. It makes things
   describe("oneOrMore", () => {
     it("should raise an error if it doesn't match anything", () => {
       // Given a new base parser with some input
